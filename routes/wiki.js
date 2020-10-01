fe0cc4d856884fe4c0201e78762f1db04fae71af
Josie Chen <josieyxchen@gmail.com>
2020-10-01 16:56:23 -0400

    Showing a Wiki Page

--- routes/wiki.js
+++ routes/wiki.js
@@ -8,25 +8,35 @@ router.get('/', (req, res) => {
 })
 
 
-
 router.post('/', async (req,res,next)=>{
   const title = req.body.title[2];
-  console.log("Title is ", title);
+  // console.log("Title is ", title);
   const content = req.body.title[3];
-  console.log("Console is ",content);
+  // console.log("Console is ",content);
 
   try{
     const page = await Page.create({
       title,
       content,
     });
+    await page.save();
     res.redirect('/');
-
-  }catch(error){next(error)}
+  } catch (error) {next(error)}
 })
 
 router.get('/add', (req, res) => {
   res.send(views.addPage());
+})
+
+router.get('/:slug', async (req, res, next) => {
+  const slug = req.params.slug;
+  const page = await Page.findOne({
+    where: {
+      slug: slug,
+    }
+  })
+  views.wikiPage(page);
+  res.redirect(`/${slug}`);
 })
 
 module.exports = router;
